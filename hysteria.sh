b2e1036da1cfc42f548a8a91df9afd6270b31f5d
Ali <134008782+imTheSabrali@users.noreply.github.com>
2023-08-27 18:03:05 +0000

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -14,7 +14,7 @@ print_with_delay() {
 # Introduction animation
 echo ""
 echo ""
-print_with_delay "hysteria-installer by DEATHLINE | @NamelesGhoul" 0.1
+print_with_delay "hysteria-installer by ImTheSabrali" 0.1
 echo ""
 echo ""
 
